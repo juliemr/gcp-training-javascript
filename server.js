998f8c00e5bc316ac71b8662329ceaa23825668e
juliemr <ju.ralph@gmail.com>
2025-04-24 16:54:23 +0000

    Making a dev commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   console.log('Request for /');
-  res.status(200).json({ message: 'Hello Julie!' });
+  res.status(200).json({ message: 'Hello Julie <dev changes>!' });
 });
 
 app.get('/health', (req, res) => {
